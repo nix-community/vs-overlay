154b0b8eeb8d383c129048931670b4552459e1c7
Simon Bruder <simon@sbruder.de>
2021-10-12 21:41:08 +0200

    vapoursynthPlugins.placebo: 1.3.1 -> 1.4.2
    This makes debanding work for the first time.

--- plugins/placebo/default.nix
+++ plugins/placebo/default.nix
@@ -2,27 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "vs-placebo";
-  version = "1.3.1";
+  version = "1.4.2";
 
   src = fetchFromGitHub {
     owner = "Lypheo";
     repo = pname;
     rev = version;
-    sha256 = "079i4ixm6273gy9x80ij7s645v5bm53vczkky5lg4vls5sk00hk2";
+    sha256 = "sha256-nerS1z/Ch/UqcmcY2gNL1Xl3hs1/etEAODj8pzrSuEE=";
     fetchSubmodules = true;
   };
-
-  patches = [
-    # fix build with newer libplacebo
-    (fetchpatch {
-      url = "https://github.com/Lypheo/vs-placebo/commit/d717bb49ce03ea9d67152a7c2e4df026de040c35.diff";
-      sha256 = "0phnhpv7alxyw4ki3kj3a19j8qv16hww6gz06lyiyabj20xqibri";
-    })
-    (fetchpatch {
-      url = "https://github.com/Lypheo/vs-placebo/commit/6a99aa1c427f9119d695d3e9d1a08782f51740d1.diff";
-      sha256 = "1x7by89vc0368p4baqlyqi5kw38plr024g4kxbr6afg4f8q4rl58";
-    })
-  ];
 
   nativeBuildInputs = [ meson ninja pkg-config ];
   buildInputs = [ libplacebo vapoursynth vulkan-headers vulkan-loader ];
