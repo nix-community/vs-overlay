c236668afbbf15caa83cb17a47b3d896ac32667c
Simon Bruder <simon@sbruder.de>
2021-10-15 18:35:48 +0200

    vapoursynthPlugins.imwri: init at R1

---
+++ plugins/imwri/default.nix
@@ -0,0 +1,29 @@
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, imagemagick, libheif, vapoursynth }:
+
+stdenv.mkDerivation rec {
+  pname = "vs-imwri";
+  version = "R1";
+
+  src = fetchFromGitHub {
+    owner = "vapoursynth";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-3nNX7OxAwHPJ6JwaTZJTH13eWktPI/XBmEC/OETCun4=";
+  };
+
+  nativeBuildInputs = [ meson ninja pkg-config ];
+  buildInputs = [ imagemagick libheif vapoursynth ];
+
+  postPatch = ''
+    substituteInPlace meson.build \
+        --replace "vapoursynth_dep.get_variable(pkgconfig: 'libdir')" "get_option('libdir')"
+  '';
+
+  meta = with lib; {
+    description = "Image reader and writer for VapourSynth using the ImageMagick library";
+    homepage = "https://github.com/vapoursynth/vs-imwri";
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [ sbruder ];
+    platforms = platforms.all;
+  };
+}
