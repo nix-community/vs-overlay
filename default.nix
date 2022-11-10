a3deb1339053a7ccf834dce3effd1b7aa16d882c
Aidan Gauland <aidalgol@fastmail.net>
2022-11-11 08:32:59 +1300

    dfmderainbow: init at unstable-10-17-2022

--- default.nix
+++ default.nix
@@ -72,6 +72,7 @@ in
     adjust = callPythonPackage ./plugins/adjust { };
     astdr = callPythonPackage ./plugins/astdr { };
     debandshit = callPythonPackage ./plugins/debandshit { };
+    dfmderainbow = callPythonPackage ./plugins/dfmderainbow { };
     edi_rpow2 = callPythonPackage ./plugins/edi_rpow2 { };
     finedehalo = callPythonPackage ./plugins/finedehalo { };
     mt_lutspa = callPythonPackage ./plugins/mt_lutspa { };
